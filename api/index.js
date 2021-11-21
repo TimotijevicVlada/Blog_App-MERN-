59ecdbd249c03284c47cc4a9f617d219fef665a4
Vladimir <timotijevicvlada@gmail.com>
2021-11-21 22:52:09 +0100

    Start to work with Node.js and MongoDB

---
+++ api/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true   //This trow some error in the terminal
+}).then(console.log("Conntected to MongoDB")).catch(err => console.log(err));
+
+app.use("/", (req, res) => {
+    console.log("Hello this is main url");
+})
+
+app.listen("5000", () => {
+    console.log("Backend is runing");
+})
