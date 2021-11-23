aaf225cc97b0f3dc69bcfd8409745894a92c5c12
Vladimir <timotijevicvlada@gmail.com>
2021-11-23 17:18:54 +0100

    Update, delete and get user done.

--- api/index.js
+++ api/index.js
@@ -3,18 +3,23 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users")
 
 dotenv.config();
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL, {
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-   // useCreateIndex: true   //This trow some error in the terminal
-}).then(console.log("Conntected to MongoDB")).catch(err => console.log(err));
+    //useCreateIndex: true   //This trow some error in the terminal
+  })
+  .then(console.log("Conntected to MongoDB"))
+  .catch((err) => console.log(err));
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen("5000", () => {
-    console.log("Backend is runing");
-})
+  console.log("Backend is runing");
+});
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -3,7 +3,7 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
 //REGISTER
-router.post("./register", async (req, res) => {
+router.post("/register", async (req, res) => {
     try {
         const salt = await bcrypt.genSalt(10);
         const hashedPass = await bcrypt.hash(req.body.password, salt);
@@ -25,11 +25,11 @@ router.post("./register", async (req, res) => {
 //LOGIN
 router.post("/login", async (req, res) => {
     try {
-        const user = User.findOne({username: req.body.username})
+        const user = await User.findOne({username: req.body.username})
         !user && res.status(400).json("Wrong credentials!")
 
-        const validate = await bcrypt.compare(req.body.password, user.password)
-        !validate && res.status(400).json("Wrong credentials!");
+        const validated = await bcrypt.compare(req.body.password, user.password)
+        !validated && res.status(400).json("Wrong credentials!");
 
         const {password, ...others} = user._doc;
         res.status(200).json(others);
@@ -43,4 +43,4 @@ router.post("/login", async (req, res) => {
 
 
 
-module.exports = router;
+module.exports = router
