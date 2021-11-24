51050a959ba2a6c452d982a3aac152937a31208b
Vladimir <timotijevicvlada@gmail.com>
2021-11-24 18:17:16 +0100

    Posts CRUD done.

--- api/index.js
+++ api/index.js
@@ -3,7 +3,8 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
-const userRoute = require("./routes/users")
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
 
 dotenv.config();
 app.use(express.json());
@@ -19,7 +20,8 @@ mongoose
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 
 app.listen("5000", () => {
-  console.log("Backend is runing");
+  console.log("Backend is running");
 });
