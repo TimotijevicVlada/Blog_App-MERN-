8af63f8ab2f39e7107ae412624c62e76f1eb136e
Vladimir <timotijevicvlada@gmail.com>
2021-11-29 17:35:05 +0100

    Signup nad Login functionality done.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,26 +1,56 @@
-import React from 'react';
+import React, { useRef, useContext } from "react";
+import { Context } from "../context/Context";
+import axios from "axios";
 
 const Login = () => {
-    return (
-        <div className="login">
-            <form className="login_form">
-                <div className="login_title">
-                    <h2>LOGIN</h2>
-                </div>
-                <div className="login_inputs">
-                    <div className="login_email">
-                        <input type="text" placeholder="Enter your email"/>
-                    </div>
-                    <div className="login_pass">
-                        <input type="password" placeholder="Enter your password"/>
-                    </div>
-                    <div className="login_btn">
-                        <button>Login</button>
-                    </div>
-                </div>
-            </form>
+  const userRef = useRef();
+  const passwordRef = useRef();
+  const { user, dispatch, isFetching } = useContext(Context);
+
+  const handleSubmit = async (e) => {
+    e.preventDefalut();
+    dispatch({type: "LOGIN_START"});
+    try {
+        const response = await axios.post("/auth/login", {
+            username: userRef.current.value,
+            password: passwordRef.current.value
+        })
+        dispatch({type: "LOGIN_SUCCESS", payload: response.data});
+    } catch (err) {
+        dispatch({type: "LOGIN_FAILURE"});
+    }
+  };
+
+  console.log(user)
+
+  return (
+    <div className="login">
+      <form onSubmit={handleSubmit} className="login_form">
+        <div className="login_title">
+          <h2>LOGIN</h2>
         </div>
-    )
-}
+        <div className="login_inputs">
+          <div className="login_email">
+            <input
+              type="text"
+              placeholder="Enter your username"
+              ref={userRef}
+            />
+          </div>
+          <div className="login_pass">
+            <input
+              type="password"
+              placeholder="Enter your password"
+              ref={passwordRef}
+            />
+          </div>
+          <div className="login_btn">
+            <button type="submit">Login</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,29 +1,63 @@
-import React from 'react';
+import React, { useState } from "react";
+import axios from "axios";
 
 const Register = () => {
-    return (
-        <div className="register">
-            <form className="register_form">
-                <div className="register_title">
-                    <h2>REGISTER</h2>
-                </div>
-                <div className="register_inputs">
-                    <div className="register_username">
-                        <input type="text" placeholder="Enter your username"/>
-                    </div>
-                    <div className="register_email">
-                        <input type="text" placeholder="Enter your email"/>
-                    </div>
-                    <div className="register_pass">
-                        <input type="password" placeholder="Enter your password"/>
-                    </div>
-                    <div className="register_btn">
-                        <button>Register</button>
-                    </div>
-                </div>
-            </form>
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("/auth/register", {
+        username,
+        email,
+        password,
+      });
+
+      response.data && window.location.replace("/login");
+    } catch (err) {
+      setError(true);
+    }
+  };
+
+  return (
+    <div className="register">
+      <form className="register_form" onSubmit={handleSubmit}>
+        <div className="register_title">
+          <h2>REGISTER</h2>
         </div>
-    )
-}
+        <div className="register_inputs">
+          <div className="register_username">
+            <input
+              onChange={(e) => setUsername(e.target.value)}
+              type="text"
+              placeholder="Enter your username"
+            />
+          </div>
+          <div className="register_email">
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              type="text"
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="register_pass">
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              placeholder="Enter your password"
+            />
+          </div>
+          <div className="register_btn">
+            <button type="submit">Register</button>
+          </div>
+          {error && <span className="error" style={{color: "red"}}>Something went wrong!</span>}
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default Register;
