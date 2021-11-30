31109b92303b5b9116acb71afccf54668d2a6de9
Vladimir <timotijevicvlada@gmail.com>
2021-11-30 17:18:10 +0100

    Delete post functionality done.

--- client/src/components/SignlePost.js
+++ client/src/components/SignlePost.js
@@ -1,12 +1,18 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState, useCallback, useContext } from "react";
 import { useLocation } from "react-router";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { Context } from "../context/Context";
 
 const SignlePost = () => {
   const location = useLocation();
   const path = location.pathname.split("/")[2];
   const [post, setPost] = useState([]);
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [updateMode, setUpdateMode] = useState(false);
+
+  const {user} = useContext(Context);
 
   //Public folder
   const PF = "http://localhost:5000/images/";
@@ -20,15 +26,28 @@ const SignlePost = () => {
     getPost();
   }, [getPost]);
 
+  const handleDelete = async () => {
+    try {
+        await axios.delete(`/posts/${post._id}`, {
+          data: {username: user.username}
+        });
+        window.location.replace("/");
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
   return (
     <div className="single_post">
       <div className="single_post_image">
         {post.photo && <img src={PF + post.photo} alt="" />}
       </div>
+      {post.username === user?.username && 
       <div className="single_post_events">
-        <i className="far fa-edit"></i>
-        <i className="fas fa-trash-alt"></i>
+        <i className="far fa-edit" onClick={() => setUpdateMode(true)}></i>
+        <i className="fas fa-trash-alt" onClick={handleDelete}></i>
       </div>
+      }
       <div className="single_post_title">
         <h2>{post.title}</h2>
       </div>
