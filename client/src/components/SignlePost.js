3445f42e2ebff6b206e4d54eb9f784138f2f6d06
Vladimir <timotijevicvlada@gmail.com>
2021-12-01 12:37:48 +0100

    Update post functionality done.

--- client/src/components/SignlePost.js
+++ client/src/components/SignlePost.js
@@ -12,7 +12,7 @@ const SignlePost = () => {
   const [desc, setDesc] = useState("");
   const [updateMode, setUpdateMode] = useState(false);
 
-  const {user} = useContext(Context);
+  const { user } = useContext(Context);
 
   //Public folder
   const PF = "http://localhost:5000/images/";
@@ -20,6 +20,8 @@ const SignlePost = () => {
   const getPost = useCallback(async () => {
     const response = await axios.get("/posts/" + path);
     setPost(response.data);
+    setTitle(response.data.title);
+    setDesc(response.data.desc);
   }, [path]);
 
   useEffect(() => {
@@ -28,39 +30,69 @@ const SignlePost = () => {
 
   const handleDelete = async () => {
     try {
-        await axios.delete(`/posts/${post._id}`, {
-          data: {username: user.username}
-        });
-        window.location.replace("/");
-    } catch(err) {
+      await axios.delete(`/posts/${post._id}`, {
+        data: { username: user.username },
+      });
+      window.location.replace("/");
+    } catch (err) {
       console.log(err);
     }
-  }
+  };
+
+  const handleUpdate = async () => {
+    try {
+      await axios.put(`/posts/${post._id}`, {
+        username: user.username,
+        title: title,
+        desc: desc,
+      });
+      setUpdateMode(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className="single_post">
       <div className="single_post_image">
         {post.photo && <img src={PF + post.photo} alt="" />}
       </div>
-      {post.username === user?.username && 
-      <div className="single_post_events">
-        <i className="far fa-edit" onClick={() => setUpdateMode(true)}></i>
-        <i className="fas fa-trash-alt" onClick={handleDelete}></i>
-      </div>
-      }
-      <div className="single_post_title">
-        <h2>{post.title}</h2>
-      </div>
+      {post.username === user?.username && (
+        <div className="single_post_events">
+          <i className="far fa-edit" onClick={() => setUpdateMode(true)}></i>
+          <i className="fas fa-trash-alt" onClick={handleDelete}></i>
+        </div>
+      )}
+      {updateMode ? (
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      ) : (
+        <div className="single_post_title">
+          <h2>{title}</h2>
+        </div>
+      )}
       <div className="single_post_author_time">
         <Link to={`/?user=${post.username}`} className="link">
           <span>Author: {post.username}</span>
         </Link>
-        
+
         <span>{new Date(post.createdAt).toDateString()}</span>
       </div>
       <div className="single_post_text">
-        <p>{post.desc}</p>
+        {updateMode ? (
+          <textarea value={desc} onChange={(e) => setDesc(e.target.value)} />
+        ) : (
+          <p>{desc}</p>
+        )}
       </div>
+      {updateMode && (
+        <div className="update_post">
+          <button onClick={handleUpdate}>Update</button>
+        </div>
+      )}
     </div>
   );
 };
